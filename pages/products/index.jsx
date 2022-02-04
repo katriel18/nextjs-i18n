0d6960b711e6e9b7f666ef25cb7ad5b8491a6b08
Osti Katriel <ostikatriel@gmail.com>
2022-02-04 18:02:40 -0500

    products

---
+++ pages/products/index.jsx
@@ -0,0 +1,22 @@
+export default function Products({ data }) {
+  //console.log('data: ',data.length)// consola VS / consola navegador
+
+  return (
+    <div>
+      <main>
+        <h1>Products: {data.length}</h1>
+        {data.map((x) => (
+          <h2 key={x.id}>{x.id}. {x.name}</h2>
+        ))}
+      </main>
+    </div>
+  );
+}
+
+export async function getServerSideProps() {
+  const res = await fetch(`http://localhost:1337/products`);
+  const data = await res.json();
+  //console.log('data: ',data.length)// consola VS / consola navegador
+
+  return { props: { data } };
+}
